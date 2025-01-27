0c5268d537d5bb46b7b44242edb31b791bcc4e8f
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-01-27 16:16:36 +0530

    Time: 3 ms (45.77%), Space: 45.5 MB (83.17%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int max_pos = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == 0 && max_pos == i) {
+                return false;
+            }
+            max_pos = Math.max(max_pos, nums[i] + i);
+        }
+
+        if (max_pos >= n-1) {
+            return true;
+        }
+        return false;
+    }
+}
