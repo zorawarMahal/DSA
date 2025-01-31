1d60d784c881427a280974763b2fbbc222915031
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-01-31 19:35:19 +0530

    Time: 1 ms (23.34%), Space: 50.5 MB (5.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public void reverseString(char[] s) {
-        // simply use two pointers to swap the elements of the character array
+        // simply use two pointers to swap the elements of the character array. The iterative approach is better because of obvious reasons like time complexity and does not use any extra space
 
         // now using the recursive approach
         swapChar(s, 0, s.length-1);
