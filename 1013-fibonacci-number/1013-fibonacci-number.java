81e6f11a82774d79455f1c1ac46e9fe031e4e8fb
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-01-31 21:05:56 +0530

    Time: 8 ms (46.58%), Space: 40.5 MB (49.41%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        return fibo(n);
+    }
+
+
+    int fibo(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
+}
