85754769b063734f2f4662028a9b99280a5b8674
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-03-17 12:52:15 +0530

    Time: 0 ms (100%), Space: 44.4 MB (61.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,15 +15,19 @@ public class Solution {
             return false;
         }
 
-        ListNode fastP = head;
-        ListNode slowP = head;
+        ListNode fastP = head; // the fast pointer moves 2 places in each iteration
+        ListNode slowP = head; // slow pointer will move 1 place in each iteration
 
         do {
             fastP = fastP.next;
+
+            // fastP.next won't exist if there are only 2 elements therefore, this will give
+            // NullPonterException. Hence this check!
             if (fastP == null) {
                 return false;
             }
             fastP = fastP.next;
+
             slowP = slowP.next;
 
             if (fastP == slowP) {
@@ -32,5 +36,22 @@ public class Solution {
         } while (fastP != null && slowP != null);
 
         return false;
+
+
+
+        // better code
+
+        // ListNode fast = head;
+        // ListNode slow = head;
+
+        // while (fast != null && fast.next != null) {
+        //     fast = fast.next.next;
+        //     slow = slow.next;
+        //     if (fast == slow) {
+        //         return true;
+        //     }
+        // }
+        
+        // return false;
     }
 }
