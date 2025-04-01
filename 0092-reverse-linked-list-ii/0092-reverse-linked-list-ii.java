0e0b9ba0c71f11a20c15b9665fbebffffbb6145f
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-04-01 18:24:52 +0530

    Time: 0 ms (100%), Space: 41.2 MB (82.73%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,42 +1,47 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || left == right) return head;
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-
-        // Move `prev` to one node before `left`
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;
+        if (left == right) {
+            return head;
         }
 
-        // Reverse the sublist from left to right
-        ListNode curr = prev.next;
-        ListNode nextNode;
-        ListNode prevNode = null;
+        ListNode current = head;
+        ListNode p = null;
 
-        for (int i = left; i <= right; i++) {
-            nextNode = curr.next;
-            curr.next = prevNode;
-            prevNode = curr;
-            curr = nextNode;
+        // skipping the first left - 1 nodes
+        for (int i=0; current != null && i < left - 1; i++) {
+            p = current;
+            current = current.next;
         }
 
-        // Reconnect the reversed part
-        prev.next.next = curr;  // Connect tail of reversed part to `curr`
-        prev.next = prevNode;    // Connect `prev` to new head of reversed part
+        ListNode last = p;
+        ListNode newEnd = current;
 
-        return dummy.next;
+        // reverse between left and right
+        for (int i=0; current != null && i < right - left + 1; i++) {
+            ListNode nextNode = current.next;
+            current.next = p;
+            p = current;
+            current = nextNode;
+        }
+
+        // making connections with the reversed part
+        newEnd.next = current;
+        if (last != null) {
+            last.next = p;
+        } else {
+            head = p;
+        }
+
+        return head;
     }
 }
