18c832f12de6c8426600236076cb56400c25678f
Zorawar Singh Mahal <118074455+zorawarMahal@users.noreply.github.com>
2025-04-09 15:33:12 +0530

    Time: 2 ms (92.63%), Space: 47.8 MB (6.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,31 +1,46 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i=0; i < numbers.length-1; i++) {
-            int index = binarySearch(numbers, target-numbers[i], i+1, numbers.length-1);
+        // for (int i=0; i < numbers.length-1; i++) {
+        //     int index = binarySearch(numbers, target-numbers[i], i+1, numbers.length-1);
 
-            if (index != -1 && numbers[index] == target-numbers[i]) {
-                return new int[] {Math.min(index,i)+1, Math.max(index,i)+1};
+        //     if (index != -1 && numbers[index] == target-numbers[i]) {
+        //         return new int[] {Math.min(index,i)+1, Math.max(index,i)+1};
+        //     }
+        // }
+
+        // return new int[] {-1,-1};
+
+        int i = 0, j = numbers.length-1;
+
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target)  {
+                return new int[] {i+1, j+1};
+            }
+            if (numbers[i] + numbers[j] < target) {
+                i++;
+            }
+            else {
+                j--;
             }
         }
-
         return new int[] {-1,-1};
     }
 
-    private int binarySearch(int[] numbers, int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end-start)/2;
+    // private int binarySearch(int[] numbers, int target, int start, int end) {
+    //     while (start <= end) {
+    //         int mid = start + (end-start)/2;
 
-            if (target == numbers[mid]) {
-                return mid;
-            }
-            if (target < numbers[mid]) {
-                end = mid-1;
-            }
-            else {
-                start = mid+1;
-            }
-        }
+    //         if (target == numbers[mid]) {
+    //             return mid;
+    //         }
+    //         if (target < numbers[mid]) {
+    //             end = mid-1;
+    //         }
+    //         else {
+    //             start = mid+1;
+    //         }
+    //     }
 
-        return -1;
-    }
+    //     return -1;
+    // }
 }
